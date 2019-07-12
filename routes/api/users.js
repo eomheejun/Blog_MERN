4cd633d8c85d5c1a797cc8dca95646720c1298cf
Unknown <gmlwns7430@naver.com>
2019-07-12 22:17:25 +0900

    user register and avatar

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,6 +1,47 @@
 const express = require("express");
 const router = express.Router();
+const gravarta = require("gravatar");
+const bcrypt = require("bcryptjs");
 
-router.get('/test', (req, res) => res.json({msg: 'user works'}));
+const userModel = require("../../models/Users");
+
+//@route POST api/users/register
+//@desc Register user
+//@access Public
+
+router.post('/register', (req, res) => {
+    userModel 
+        .findOne({email: req.body.email})
+        .then(user => {
+            if(user) {
+                return res.status(400).json({
+                    msg: "Email 있음"
+                })
+            }else{
+                const avatar = gravarta.url(req.body.email, {
+                    s:'200', //size
+                    r:'pg',  //Rating
+                    d:'mm'  //Default
+                });
+                const newUser = new userModel({
+                    name:req.body.name,
+                    email: req.body.email,
+                    avatar,
+                    password: req.body.password
+                });
+                //password 암호화
+                bcrypt.genSalt(10, (err, salt) => {
+                    bcrypt.hash(newUser.password, salt, (err, hash) => {
+                        if(err) throw err;
+                        newUser.password = hash;
+                        newUser.save()
+                            .then(user => res.json(user))
+                            .catch(err => res.json(err));
+                    });
+                })
+            }
+        })
+        .catch(err => res.json(err));
+});
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,11 +1,19 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
 
 const usersRoutes = require("./routes/api/users");
 const profileRoutes = require("./routes/api/profile");
 const postsRoutes = require("./routes/api/posts");
 
 const app = express();
+
+//bodyparser middleware
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+
+
+
 //DB config
 const db = require("./config/keys").mongoURI;
 
@@ -23,6 +31,7 @@ mongoose
 app.use('/api/users', usersRoutes);
 app.use('/api/profile', profileRoutes);
 app.use('/api/posts', postsRoutes);
+
 
 const PORT = process.env.PORT || 3000;//따로지정하는 번호가 있으면 왼쪽
 
