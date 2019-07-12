d71265cc77b1432d1ff6d1c7f200c16d3d4889ba
Unknown <gmlwns7430@naver.com>
2019-07-12 21:40:37 +0900

    node route files

--- server.js
+++ server.js
@@ -1,7 +1,16 @@
 const express = require("express");
+const usersRoutes = require("./routes/api/users");
+const profileRoutes = require("./routes/api/profile");
+const postsRoutes = require("./routes/api/posts");
 
 const app = express();
 
-const PORT = process.env.PORT || 3000;
+//Use Routes
+app.use('/api/users', usersRoutes);
+app.use('/api/profile', profileRoutes);
+app.use('/api/posts', postsRoutes);
+
+const PORT = process.env.PORT || 3000;//따로지정하는 번호가 있으면 왼쪽
+
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
