e4e0f6f5c3a6edde7978e7fc17696b5100ad74fa
Unknown <gmlwns7430@naver.com>
2019-08-12 20:23:13 +0900

    add post posting

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -1,5 +1,48 @@
 const express = require("express");
+const mongoose = require("mongoose");
+const passport = require("passport");
 const router = express.Router();
+
+//Post model
+
+const postModel = require('../../models/post');
+
+//validation
+
+const validatePostInput = require("../../validation/post");
+
+const authcheck = passport.authenticate('jwt', {session: false});
+
+
+//@route Post api/posts
+//@desc create post
+//@access Private
+
+router.post('/', authcheck, (req, res) => {
+
+    const {errors, isValid} = validatePostInput(req.body);
+
+    //check validation
+
+    if(!isValid) {
+        return res.status(400).json(errors);
+    }
+
+    const newPost = new postModel({
+        text: req.body.text,
+        name: req.user.name,
+        avatar: req.user.avatar,
+        user: req.user.id //토큰에서받아옴
+    });
+    
+    newPost
+        .save()
+        .then(post => res.json({
+            msg: 'successful Posting',
+            post: post
+        }))
+        .catch(err => res.json(err));
+});
 
 
 module.exports = router;
