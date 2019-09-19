95e97b5a53e6c5493f02f362e56f9c8e226ed62c
Unknown <gmlwns7430@naver.com>
2019-09-19 20:05:53 +0900

    Add Footer

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,14 @@
 import React from 'react';
+import Footer from "./components/layout/Footer";
 
 
 function App() {
   return (
     <div className="App">
       <h1>hi</h1>
+      <Footer/>
     </div>
+
   );
 }
 
