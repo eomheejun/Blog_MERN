9357979fdc09b0de28c45f9e50a30207f504644d
Unknown <gmlwns7430@naver.com>
2019-09-19 20:55:09 +0900

    Add react router dom

--- client/src/App.js
+++ client/src/App.js
@@ -1,19 +1,27 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 import Footer from "./components/layout/Footer";
 import Navbar from "./components/layout/Navbar";
 import Landing from "./components/layout/Landing";
+import login from "../src/components/auth/login";
+import register from "../src/components/auth/register";
 
-
-function App() {
-  return (
-    <div className="App">
-      <Navbar/>
-      <Landing/>
-      <Footer/>
-
-    </div>
-
-  );
+class App extends Component {
+  render() {
+    return(
+      <Router>
+        <div>
+          <Navbar/>
+          <Route exact path="/" component={Landing} />
+          <div>
+            <Route exact path="/register" component={register}/>
+            <Route exact path="/login" component={login}/>
+          </div>
+          <Footer/>
+        </div>
+      </Router>
+    );
+  }
 }
 
 export default App;
---
+++ client/src/components/auth/register.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class register extends Component {
+    render() {
+        return(
+            <div>
+                <h1>Register</h1>
+            </div>
+        );
+    }
+}
+
+export default register;
